b5ae62a941703b6f5eaa7c06bf0c811d4b43e492
lê hữu võ long <longpro8e20@gmail.com>
2021-04-07 21:50:14 +0700

    First Commit

---
+++ DM1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace DM1
+{
+	class Program
+	{
+		static string GetChunks
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var interfaces = NetworkInterface.GetAllNetworkInterfaces();
+			var activeInterfaces = interfaces.Where((x) => { return x.OperationalStatus == OperationalStatus.Up; });
+
+			//var wifi = activeInterfaces.Where(x => x.Name.ToUpper() == "ETHERNET").FirstOrDefault();
+			var eth = activeInterfaces.Where(x => x.Name.ToUpper() == "ETHERNET").FirstOrDefault();
+
+			//TcpClient wifiClient = new TcpClient(
+			//	new IPEndPoint(wifi.GetIPProperties().UnicastAddresses[0].Address, 
+			//	0
+			//));
+			TcpClient ethClient = new TcpClient(
+				new IPEndPoint(
+					eth.GetIPProperties().UnicastAddresses[0].Address,
+					0
+			));
+
+			var cli = new MyHttpClient(ethClient);
+			MyHttpClient.Response response;
+			cli.Connect("api.myip.com", 443);
+			if (cli.SendRequest(new MyHttpClient.Request("api.myip.com", "/") { 
+				Method = "GET",
+				Body = ""
+			}))
+			{
+				response = cli.RecvResponse();
+				Console.WriteLine(response.Body);
+			}
+		}
+	}
+}
